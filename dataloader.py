9bc56d2a10fc3287050e24d7b5da04599b6e6ce7
Hao Chen <hchen27@nd.edu>
2024-07-25 00:28:46 -0400

    update the framework, prepare for first training

--- dataloader.py
+++ dataloader.py
@@ -1,24 +1,82 @@
 import torch
 from torch.utils.data import Dataset as TorchDataset
+from torchvision import transforms
+from PIL import Image
 import numpy as np
+import os
+
+def fluore_to_tensor(pic):
+    """Convert a ``PIL Image`` to tensor. Range stays the same.
+    Only output one channel, if RGB, convert to grayscale as well.
+    Currently data is 8 bit depth.
+    """
+    if not isinstance(pic, Image.Image):
+        raise TypeError('pic should be PIL Image. Got {}'.format(type(pic)))
+
+    # Convert PIL Image to numpy array
+    img = np.array(pic)
+    if pic.mode in ['L', 'P', '1']:
+        img = torch.from_numpy(img).unsqueeze(0)
+    elif pic.mode in ['RGB', 'RGBA']:
+        img = torch.from_numpy(np.dot(img[..., :3], [0.2989, 0.5870, 0.1140])).unsqueeze(0)
+    else:
+        raise TypeError('Unsupported image type {}'.format(pic.mode))
+
+    return img
+
+def pil_loader(path):
+    """Load an image."""
+    return Image.open(path).convert('RGB')
 
 class DNdataset(TorchDataset):
-    def __init__(self, data, labels, transform=None):
-        self.data = data
-        self.labels = labels
+    def __init__(self, root, noise_levels, types=None, transform=None, target_transform=None, loader=pil_loader, test_fov=19):
+        self.root = root
+        self.noise_levels = noise_levels
+        self.types = types
         self.transform = transform
+        self.target_transform = target_transform
+        self.loader = loader
+        self.images = self._make_dataset()
+    
+    def _make_dataset(self):
+        """Scan directories and gather image paths."""
+        images = []
+        for noise_level in self.noise_levels:
+            noise_path = os.path.join(self.root, str(noise_level))
+            if self.types:
+                for t in self.types:
+                    type_path = os.path.join(noise_path, t)
+                    for img_name in os.listdir(type_path):
+                        img_path = os.path.join(type_path, img_name)
+                        images.append(img_path)
+            else:
+                for img_name in os.listdir(noise_path):
+                    img_path = os.path.join(noise_path, img_name)
+                    images.append(img_path)
+        return images
 
     def __len__(self):
-        return len(self.data)
+        return len(self.images)
 
     def __getitem__(self, idx):
-        sample = self.data[idx]
-        label = self.labels[idx]
+        img_path = self.images[idx]
+        img = self.loader(img_path)
         if self.transform:
-            sample = self.transform(sample)
-        return sample, label
+            img = self.transform(img)
+        if self.target_transform:
+            label = self.target_transform(img)
+        else:
+            label = img  # Assuming a self-supervised task
+        return img, label
 
-def generate_data(num_samples=1000, num_classes=10, img_size=(256, 256)):
-    data = np.random.rand(num_samples, 1, *img_size).astype(np.float32)
-    labels = np.random.randint(0, num_classes, num_samples)
-    return data, labels
+def img_loader(root, batch_size, noise_levels, types=None, patch_size=256, test_fov=19):
+    """For N2N model, use all captures in each fov, randomly select 2 when loading."""
+    transform = transforms.Compose([
+        transforms.CenterCrop(patch_size),
+        fluore_to_tensor,
+        transforms.Normalize(mean=[0.5], std=[0.5])
+    ])
+    dataset = DNdataset(root, noise_levels, types=types, test_fov=test_fov, transform=transform, target_transform=transform)
+    kwargs = {'num_workers': 4, 'pin_memory': True} if torch.cuda.is_available() else {}
+    data_loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True, drop_last=False, **kwargs)
+    return data_loader
--- train.py
+++ train.py
@@ -2,29 +2,31 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.tensorboard import SummaryWriter
-from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
 from model import DNFLIM
+from dataloader import DNdataset, generate_data
+from torchvision import transforms
 
 def train_model(epochs, batch_size, lr):
+    data, labels = generate_data(num_samples=60000, img_size=(256, 256))
     transform = transforms.Compose([
         transforms.ToTensor(),
         transforms.Normalize((0.5,), (0.5,))
     ])
-
-    train_dataset = datasets.MNIST(root='./data', train=True, transform=transform, download=True)
-    train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
+    train_dataset = DNdataset(data, labels, transform=transform)
+    train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
 
     model = DNFLIM()
-    criterion = nn.CrossEntropyLoss()
+    criterion = nn.MSELoss()  # Assuming denoising task
     optimizer = optim.Adam(model.parameters(), lr=lr)
     writer = SummaryWriter('runs/DNFLIM_experiment')
 
     for epoch in range(epochs):
         running_loss = 0.0
-        for i, (inputs, labels) in enumerate(train_loader):
+        for i, (inputs, _) in enumerate(train_loader):
             optimizer.zero_grad()
             outputs = model(inputs)
-            loss = criterion(outputs, labels)
+            loss = criterion(outputs, inputs)  # Denoising target is the original input
             loss.backward()
             optimizer.step()
             running_loss += loss.item()
