0edf10e2d5cc728f8fd4fa8453201ca271dc1bec
Hao Chen <hchen27@nd.edu>
2024-07-25 00:11:30 -0400

    update model

--- model.py
+++ model.py
@@ -7,9 +7,9 @@ class DoubleConv(nn.Module):
         super(DoubleConv, self).__init__()
         self.double_conv = nn.Sequential(
             nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
+            nn.LeakyReLU(negative_slope=0.01, inplace=True),  # Using LeakyReLU
             nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True)
+            nn.LeakyReLU(negative_slope=0.01, inplace=True)   # Using LeakyReLU
         )
         
     def forward(self, x):
@@ -19,45 +19,31 @@ class UNetBranch(nn.Module):
     def __init__(self, in_channels, out_channels):
         super(UNetBranch, self).__init__()
         self.encoder = nn.Sequential(
-            DoubleConv(in_channels, 64),
+            DoubleConv(in_channels, 32),
             nn.MaxPool2d(2),
-            DoubleConv(64, 128),
+            DoubleConv(32, 64),
             nn.MaxPool2d(2),
-            DoubleConv(128, 256),
-            nn.MaxPool2d(2),
-            DoubleConv(256, 512),
-            nn.MaxPool2d(2),
-            DoubleConv(512, 1024)
+            DoubleConv(64, 128)
         )
         self.decoder = nn.Sequential(
-            nn.ConvTranspose2d(1024, 512, kernel_size=2, stride=2),
-            DoubleConv(1024, 512),
-            nn.ConvTranspose2d(512, 256, kernel_size=2, stride=2),
-            DoubleConv(512, 256),
-            nn.ConvTranspose2d(256, 128, kernel_size=2, stride=2),
-            DoubleConv(256, 128),
             nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2),
-            DoubleConv(128, 64)
+            DoubleConv(128, 64),
+            nn.ConvTranspose2d(64, 32, kernel_size=2, stride=2),
+            DoubleConv(64, 32)
         )
-        self.final_conv = nn.Conv2d(64, out_channels, kernel_size=1)
+        self.final_conv = nn.Conv2d(32, out_channels, kernel_size=1)
         
     def forward(self, x):
         enc1 = self.encoder[0](x)
         enc2 = self.encoder[1](enc1)
-        enc3 = self.encoder[2](enc2)
-        enc4 = self.encoder[3](enc3)
-        bottleneck = self.encoder[4](enc4)
+        bottleneck = self.encoder[2](enc2)
 
         dec1 = self.decoder[0](bottleneck)
-        dec2 = self.decoder[1](torch.cat((dec1, enc4), dim=1))
+        dec2 = self.decoder[1](torch.cat((dec1, enc2), dim=1))
         dec3 = self.decoder[2](dec2)
-        dec4 = self.decoder[3](torch.cat((dec3, enc3), dim=1))
-        dec5 = self.decoder[4](dec4)
-        dec6 = self.decoder[5](torch.cat((dec5, enc2), dim=1))
-        dec7 = self.decoder[6](dec6)
-        dec8 = self.decoder[7](torch.cat((dec7, enc1), dim=1))
+        dec4 = self.decoder[3](torch.cat((dec3, enc1), dim=1))
         
-        return self.final_conv(dec8)
+        return self.final_conv(dec4)
 
 class EncodeDecode(nn.Module):
     def __init__(self, in_channels, out_channels):
@@ -67,13 +53,9 @@ class EncodeDecode(nn.Module):
             nn.MaxPool2d(2),
             DoubleConv(64, 128),
             nn.MaxPool2d(2),
-            DoubleConv(128, 256),
-            nn.MaxPool2d(2),
-            DoubleConv(256, 512)
+            DoubleConv(128, 256)
         )
         self.decoder = nn.Sequential(
-            nn.ConvTranspose2d(512, 256, kernel_size=2, stride=2),
-            DoubleConv(512, 256),
             nn.ConvTranspose2d(256, 128, kernel_size=2, stride=2),
             DoubleConv(256, 128),
             nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2),
@@ -84,24 +66,21 @@ class EncodeDecode(nn.Module):
     def forward(self, x):
         enc1 = self.encoder[0](x)
         enc2 = self.encoder[1](enc1)
-        enc3 = self.encoder[2](enc2)
-        bottleneck = self.encoder[3](enc3)
+        bottleneck = self.encoder[2](enc2)
 
         dec1 = self.decoder[0](bottleneck)
-        dec2 = self.decoder[1](torch.cat((dec1, enc3), dim=1))
+        dec2 = self.decoder[1](torch.cat((dec1, enc2), dim=1))
         dec3 = self.decoder[2](dec2)
-        dec4 = self.decoder[3](torch.cat((dec3, enc2), dim=1))
-        dec5 = self.decoder[4](dec4)
-        dec6 = self.decoder[5](torch.cat((dec5, enc1), dim=1))
+        dec4 = self.decoder[3](torch.cat((dec3, enc1), dim=1))
 
-        return self.final_conv(dec6)
+        return self.final_conv(dec4)
 
 class DNFLIM(nn.Module):
     def __init__(self):
         super(DNFLIM, self).__init__()
-        self.branch1 = UNetBranch(in_channels=1, out_channels=64)
-        self.branch2 = UNetBranch(in_channels=1, out_channels=64)
-        self.denoise_net = EncodeDecode(in_channels=128, out_channels=1)
+        self.branch1 = UNetBranch(in_channels=1, out_channels=32)
+        self.branch2 = UNetBranch(in_channels=1, out_channels=32)
+        self.denoise_net = EncodeDecode(in_channels=64, out_channels=1)
         
     def forward(self, x):
         branch1_output = self.branch1(x)
@@ -109,3 +88,4 @@ class DNFLIM(nn.Module):
         concatenated_output = torch.cat((branch1_output, branch2_output), dim=1)
         denoised_output = self.denoise_net(concatenated_output)
         return denoised_output
+
